f8cf278137875e0b6db4200737df688516ac03fd
Ali Mahdi <seyyedalimehdi@gmail.com>
2024-09-07 23:13:54 +0330

    Update middlewere.

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -7,7 +7,7 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
 
-  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WRBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
@@ -71,9 +71,11 @@ export async function POST(req: Request) {
     return NextResponse.json({ message: "OK", mongoUser });
   }
 
+  console.log({ eventType });
   if (eventType === "user.updated") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
+    console.log(email_addresses);
 
     // Create new user in your database
     const updatedUser = await updateUser({
